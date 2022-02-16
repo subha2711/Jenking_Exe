f17eaaffa0c44eaa827c7750b672e756e4fbf04d
subha2711 <suba.pr2711@gmail.com>
2022-02-16 11:31:11 -0600

    secong commit

---
+++ src/main/java/jen_practice/Jenkins_practice.java
@@ -0,0 +1,10 @@
+package jen_practice;
+
+public class Jenkins_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("pushing to see if it is getting updated in jenkins");
+	}
+
+}
